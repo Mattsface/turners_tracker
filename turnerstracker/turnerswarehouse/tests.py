00f470a710edad8f2a3df7e6f126cd4210ecf1e3
MattsFace <spahmatthew@gmail.com>
2015-07-04 01:30:45 -0700

    Trying to write tests

--- turnerstracker/turnerswarehouse/tests.py
+++ turnerstracker/turnerswarehouse/tests.py
@@ -10,6 +10,13 @@ class CategoryTestCase(TestCase):
         actual = unicode(c1)
         self.assertEqual(expected, actual)
 
+    def test_bad_catergry(self):
+        expected = ""
+        c1 = Category(name="")
+        actual = unicode(c1)
+        c1.save()
+        self.assertEqual(expected, actual)
+
 
 class ItemTestCase(TestCase):
     fixtures = ['test_models_turners.json']
